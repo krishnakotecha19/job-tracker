8b082b79a85eb9986f07fa8b2161b8fff532fcd9
krishnakotecha19 <kotechakrishnaa@gmail.com>
2025-04-08 23:48:55 +0530

    Initial commit with frontend files

--- Procfile
+++ Procfile
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 web: gunicorn app:app
+=======
+web: python app.py
+>>>>>>> 1818d04 (Add Procfile for Railway start command)
--- db_config.py
+++ db_config.py
@@ -1,11 +1,10 @@
 from flask_mysqldb import MySQL
 
 def init_db(app):
-    app.config['MYSQL_HOST'] = 'switchback.proxy.rlwy.net'
-    app.config['MYSQL_PORT'] = 23169
+    app.config['MYSQL_HOST'] = 'shuttle.proxy.rlwy.net'
+    app.config['MYSQL_PORT'] = 12271
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'UGWIQalgOjSOaawYgFiUDuTIjYGUETmo'
+    app.config['MYSQL_PASSWORD'] = 'PkvrsVLVfKUEqmQUwlkLtzVZGMHMFXwt'
     app.config['MYSQL_DB'] = 'railway'
-    
-    mysql = MySQL(app)
-    return mysql
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    return MySQL(app)
